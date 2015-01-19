41add40e2c3ca8bb23b81342b95dce06261e33a4
yorito <orito.yoshiyuki@gmail.com>
2015-01-19 16:43:53 +0900

    選択、再生、停止ができるようになった

---
+++ MusicPlayerSampleSwift/MusicPlayerSampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayerSampleSwift
+//
+//  Created by ORITO YOSHIYUKI on 2015/01/19.
+//  Copyright (c) 2015年 musicplayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

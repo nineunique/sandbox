e6d182e6db50a6de2ed1ded41952bf4f81636c73
yorito <orito.yoshiyuki@gmail.com>
2015-01-19 15:56:19 +0900

    カスタムクラスを作ってButtonを真円に表示

---
+++ CircleButtonSampleSwift/CircleButtonSampleSwift/MyCircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  CircleButtonSampleSwift
+//
+//  Created by ORITO YOSHIYUKI on 2015/01/19.
+//  Copyright (c) 2015年 circlebutton. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CircleButtonSampleSwift/CircleButtonSampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleButtonSampleSwift
+//
+//  Created by ORITO YOSHIYUKI on 2015/01/19.
+//  Copyright (c) 2015年 circlebutton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
